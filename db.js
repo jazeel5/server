cd56534fb19d8cd02059a6e9eb0dded42d3f6ee2
jazeel5 <mohammedjazeel5@gmail.com>
2024-07-28 17:35:18 +0530

    db.js change

--- db.js
+++ db.js
@@ -1,33 +1,14 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const env = require("dotenv");
-env.config();
+const mongoose = require("mongoose");
+const env = require('dotenv')
+env.config()
 
-// MONGO_URI="mongodb+srv://gauthamkotian020:RbsFEavCpsZ3H3KP@login-app.5m8yqde.mongodb.net/?retryWrites=true&w=majority&appName=login-app"
-
-const uri = process.env.MONGO_URI;
-
-let client;
-let clientPromise;
-
-if (!client) {
-  client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  });
-  clientPromise = client.connect();
-}
-
-const connectToMongo = async () => {
+const connectMongoDb = async () => {
   try {
-    await clientPromise;
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (err) {
-    console.error("Failed to connect to MongoDB", err);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.log(error);
   }
 };
 
-module.exports = connectToMongo;
+module.exports = connectMongoDb;
